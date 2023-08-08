12813c1b2714b4cb3c5870baebb9619efb83db60
LA Katigbak <lakatigbak31@gmail.com>
2023-08-08 15:43:52 +0800

    Many Updated to be continued

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       colors:{
         'fwhite-light':'rgb(241,248,245)',
-        'fwhite-dark':'rgb(212,233,226)'
+        'fwhite-dark':'rgb(212,233,226)',
+        'cream': '#F2F0EB'
       },
       backgroundImage:{
         'star':"url('../img/img/rewards/star.png')"
