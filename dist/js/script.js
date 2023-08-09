6c5781ba7e424e8e4617298570d6c4e79e5002a2
LA Katigbak <lakatigbak31@gmail.com>
2023-08-09 15:40:59 +0800

    starbucks tbc

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,28 +1,57 @@
-const kalboNav = document.querySelectorAll(".kalbo__nav__link");
-const kalboContent = document.querySelectorAll(".kalbo__item__content");
-kalboNav.forEach((kalbo) => {
-    kalbo.addEventListener("click", () => {
-    removeActiveKalbo();
-    kalbo.style = "transition:.5s ease";
-    kalbo.classList.add("active");
-    kalbo.style = "transition:.5s ease";
-    const activeContent = document.querySelector(`#${kalbo.id}-content`);
+const favoritesNav= document.querySelectorAll(".favorites__nav__link");
+const faveContent = document.querySelectorAll(".fav__item__content");
+favoritesNav.forEach((favorites) => {
+    favorites.addEventListener("click", () => {
+    removeActiveFavorites();
+    favorites.style = "transition:.5s ease";
+    favorites.classList.add("active");
+    favorites.style = "transition:.5s ease";
+    const activeContent = document.querySelector(`#${favorites.id}-content`);
     removeActiveContent();
     activeContent.classList.add("active");
  });
 });
 
-function removeActiveKalbo() {
-  kalboNav.forEach((kalbo) => {
-    kalbo.classList.remove("active");
+function removeActiveFavorites() {
+  favoritesNav.forEach((favorites) => {
+    favorites.classList.remove("active");
   });
 }
 
 function removeActiveContent() {
-  kalboContent.forEach((kalbo) => {
-  kalbo.classList.remove("active");
+  faveContent.forEach((favorites) => {
+  favorites.classList.remove("active");
   });
 }
+
+
+
+
+// const kalboNav = document.querySelectorAll(".kalbo__nav__link");
+// const kalboContent = document.querySelectorAll(".kalbo__item__content");
+// kalboNav.forEach((kalbo) => {
+//     kalbo.addEventListener("click", () => {
+//     removeActiveKalbo();
+//     kalbo.style = "transition:.5s ease";
+//     kalbo.classList.add("active");
+//     kalbo.style = "transition:.5s ease";
+//     const activeContent = document.querySelector(`#${kalbo.id}-content`);
+//     removeActiveContent();
+//     activeContent.classList.add("active");
+//  });
+// });
+
+// function removeActiveKalbo() {
+//   kalboNav.forEach((kalbo) => {
+//     kalbo.classList.remove("active");
+//   });
+// }
+
+// function removeActiveContent() {
+//   kalboContent.forEach((kalbo) => {
+//   kalbo.classList.remove("active");
+//   });
+// }
 
 const headerNav = document.querySelector(".header__nav");
 const toggleMenu = document.querySelector(".toggle__menu");
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,13 @@ module.exports = {
       colors:{
         'fwhite-light':'rgb(241,248,245)',
         'fwhite-dark':'rgb(212,233,226)',
+        'berde':'#006341',
+        'berde2': '#d4e9e2',
         'cream': '#F2F0EB'
       },
       backgroundImage:{
-        'star':"url('../img/img/rewards/star.png')"
+        'star':"url('../img/img/rewards/star.png')",
+        'graffiti': "url('../img/graffiti.webp')"
       }
     },
   },
