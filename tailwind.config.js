e786c8eeb1390f327003d75e0de514ea132c2414
LA Katigbak <lakatigbak31@gmail.com>
2023-08-10 15:22:57 +0800

    Starbucks Home and Rewards Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
         'fwhite-dark':'rgb(212,233,226)',
         'berde':'#006341',
         'berde2': '#d4e9e2',
-        'cream': '#F2F0EB'
+        'cream': '#F2F0EB',
+        'fenk': '#EB81A5',
+        'bayolet': '#8F8BF4',
+        'ngima': '#F1FF67'
+
       },
       backgroundImage:{
         'star':"url('../img/img/rewards/star.png')",
